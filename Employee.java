51f8767211407c418fcf371b45f15a053935903f
Annn2201 <113761537+Annn2201@users.noreply.github.com>
2022-09-30 13:25:15 +0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+  String empName = "John";
+  int empId = 5;
+  int empAge = 32;
+  public static void main(String[] args) {
+    Employee empObj = new Employee();
+    System.out.println(empObj.empName);
+    System.out.println(empObj.empId);
+    System.out.println(empObj.empAge);
+  }
+}
